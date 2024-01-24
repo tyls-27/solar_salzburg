ebd32427163d519eccfd8a01f9f686436de85f73
tyls-27 <122211858+tyls-27@users.noreply.github.com>
2024-01-24 14:04:51 +0000

    Add files via upload

--- geodatenverarbeitung_tim.R
+++ geodatenverarbeitung_tim.R
@@ -57,6 +57,9 @@ bundesland <- gem_grenz_raw %>%
 # f <- file.path("daten_zwischenablage", "lc_sbg.tif") # define file path
 # writeRaster(lc_sbg, f, overwrite = T) # write out raster
 
+# read in data on zoning ----
+fl_wid_raw <- st_read("offline_files/Flaechenwidmung/Flaechenwidmung.shp")
+
 # read in dsm ----
 dgm <- rast("offline_files/dgm5m/dgm5m.asc")
 
@@ -118,7 +121,7 @@ pop_raw <- read_csv("https://raw.githubusercontent.com/tyls-27/solar_salzburg/ma
 
 # download data from OSM on roads ----
 # define bounding box based on bb from bundesland sf object
-osm_bbox <- st_bbox(bundesland)
+osm_bbox <- st_bbox(st_transform(bundesland, "EPSG: 4326"))
 # pass our bounding box coordinates into the OverPassQuery (opq) function
 #sbg_strassen_osm <- opq(bbox = osm_bbox) %>%
   # pipe this into the add_osm_feature data query function to extract our roads
@@ -157,16 +160,16 @@ osm_bbox <- st_bbox(bundesland)
 #  st_transform("EPSG: 31258") # reproject to working CRS
 
 # merge the two spatial dfs into one spatial df
-# sbg_str_comb <- bind_rows(sbg_str_main, sbg_str_link)
+# sbg_str_all <- bind_rows(sbg_str_main, sbg_str_link)
 
 # write out shp
-# st_write(sbg_str_comb, "daten_zwischenablage/sbg_strassen.shp", append = F)
+# st_write(sbg_str_all, "daten_zwischenablage/sbg_strassen.shp", append = F)
 
 # load in shp on road data in Salzburg
-sbg_str_comb <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/sbg_strassen.shp")
+sbg_str_all <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/sbg_strassen.shp")
 
 # clip to size of Salburg province
-sbg_str_comb <- st_intersection(sbg_str_comb, bundesland) # clip using Salzburg state polygon
+sbg_str_all <- st_intersection(sbg_str_comb, bundesland) # clip using Salzburg state polygon
 
 # download data from OSM on power lines (work out if osm or sbg data is better)----
 # pass our bounding box coordinates into the OverPassQuery (opq) function
@@ -381,7 +384,6 @@ sbg_bahn <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_sa
 # clip to size of Salburg province
 sbg_bahn <- st_intersection(sbg_bahn, bundesland) # clip using Salzburg state polygon
 
-
 #### ------------------------ Further Data Cleaning ----------------------- ####
 # clean the column names of all dataframes and spatial data frames ----
 # Get a list of all data frames in the global environment
@@ -404,23 +406,22 @@ for (df_name in names(df_list)) {
 # merge together population data with geometry data of muncipality ----
 
 # aggregate Kastralgemeinde population to the Gemeinde (municipal) level 
-pop <- pop_clean %>% 
+gemeinden <- pop_clean %>% 
   filter(bundesland == "Salzburg") %>%
   select(-c(4,5)) %>%
   rename(c(gkz = 2, bevölkerung = 4)) %>%
   group_by(gemeindename, gkz) %>%
-  summarise(bevölkerung = sum(bevölkerung))
-
-# merge pop_clean and gem_grenz_raw on basis of muncipality name & code
-gemeinden <- gem_grenz_clean %>%
-  left_join(pop_clean, by = c("gemnr" = "gkz", "name" = "gemeindename"))
+  summarise(bevölkerung = sum(bevölkerung)) %>%
+  # merge with gem_grenz_raw on basis of muncipality name & code
+  right_join(gem_grenz_clean, by = c("gkz" = "gemnr", "gemeindename" = "name")) %>%
+  st_as_sf() # reasign as sf object
 
 # merge datasets on protected areas together ----
 
 # merge cleaned datasets using bind_rows 
-pa_comb <- bind_rows(ffh_clean, geschuezte_lt_clean, geschuezte_ng_clean, lsg_clean, 
-                     natura2000_clean, nd_clean, np_clean, nsg_clean, psg_clean, 
-                     ramsar_clean, spa_clean, wsg_clean)
+pa_all <- bind_rows(ffh_clean, geschuezte_lt_clean, geschuezte_ng_clean, 
+                    lsg_clean, natura2000_clean, nd_clean, np_clean, nsg_clean, 
+                    psg_clean, ramsar_clean, spa_clean, wsg_clean)
 
 # check osm ids ----
 
@@ -445,11 +446,276 @@ any(duplicated(sbg_wohngebiete_clean$osm_id)) # false
 
 # double check all of the other datasets (no need to leave code) ----
 # combine nature conservation areas into one polygon - check for overaps and then combine - fuse borders
+# extract water, roads, train tracks, solar parks, populated areas, ---- 
+# enterprise areas, and residential areas from fl_wid_clean
+
+# check validity of all geometries and correct if not
+if ("geometry" %in% names(fl_wid_clean)) {
+  
+  # Check validity of geometries
+  invalid_geoms <- st_is_valid(fl_wid_clean$geometry)
+  
+  # If there are invalid geometries, correct them using st_make_valid
+  if (any(!invalid_geoms)) {
+    # Identify and correct invalid geometries
+    fl_wid_clean$geometry[!invalid_geoms] <- st_make_valid(fl_wid_clean$geometry[!invalid_geoms])
+    
+  }
+  
+}
+
+# extra data for water, roads, train tracks, solar parks, populated areas, and 
+# enterprise areas and add a key for these categories
+
+fl_wid_bewohnt <- fl_wid_clean %>%
+  filter(typname == "BARW" | typname == "BAEW" | typname == "BAFW" | 
+           typname == "BAKG" | typname == "BALK" | typname == "BADG" | 
+           typname == "BABE" | typname == "BAGG" | typname == "BAIG" | 
+           typname == "BAZG" | typname == "BAHV" | typname == "BAHC" | 
+           typname == "BAHF" | typname == "BAHB" | typname == "BAHE" | 
+           typname == "BABG") %>%
+  mutate(kategorie = "bewohnt")
+
+fl_wid_wohngebiete <- fl_wid_clean %>%
+  filter(typname == "BARW" | typname == "BAEW" | typname == "BAFW" | 
+           typname == "BAKG" | typname == "BALK" | typname == "BADG" | 
+           typname == "BAZG" | typname == "BABG") %>%
+  mutate(kategorie = "wohngebiet")
+
+fl_wid_betriebe <- fl_wid_clean %>%
+  filter(typname == "BADG" | typname == "BABE" | typname == "BAGG" | 
+           typname == "BAIG" | typname == "BAHV" | typname == "BAHC" | 
+           typname == "BAHF" | typname == "BAHB" | typname == "BAHE" | 
+           typname == "BABG" | typname == "GLMG") %>%
+  mutate(kategorie = "betrieb")
+
+fl_wid_gewaesser <- fl_wid_clean %>%
+  filter(typname == "GLGG") %>%
+  mutate(kategorie = "gewaesser")
+
+fl_wid_bahn <- fl_wid_clean %>%
+  filter(typname == "VEEB") %>%
+  mutate(kategorie = "bahn")
+
+fl_wid_str <- fl_wid_clean %>%
+  filter(typname == "VEGM" | typname == "VEBL" | typname == "VESO") %>%
+  mutate(kategorie = "strasse")
+
+fl_wid_solar <- fl_wid_clean %>%
+  filter(typname == "GLSA") %>%
+  mutate(kategorie = "solar")
+
+# combine these into a new spatial dataframe with a key for 
+fl_wid_kat_alle <- bind_rows(list(fl_wid_bahn, fl_wid_betriebe, fl_wid_bewohnt, 
+                                  fl_wid_gewaesser, fl_wid_str, 
+                                  fl_wid_wohngebiete, fl_wid_solar))
+
+#### -------------------------------- Roads ------------------------------- ####
+# create buffer around roads
+str_buffer <- fl_wid_kat_alle %>%
+  filter(kategorie == "strasse") %>% # choose roads from df
+  select(geometry) %>% # select only the geometry column for spatial operations
+  st_union() %>% # combine all road geometries into one polygon geometry
+  st_buffer(500)%>% # 500 m buffer around geometries
+  st_intersection(bundesland_clean) # clip to size of state
+
+# write str_buffer to a shapefile
+st_write(str_buffer, "daten_zwischenablage/str_buffer.shp", append = F)
+
+# read in str_buffer
+str_buffer <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/str_buffer.shp")
+
+#### --------------------------- Populated Areas -------------------------- ####
+# create buffer around populated areas
+# combine all populated area geometries into one polygon geometry
+bewohnt_buffer <- fl_wid_kat_alle %>%
+  filter(kategorie == "bewohnt") %>% # choose populated areas from df
+  select(geometry) %>% # select only the geometry column for spatial operations
+  st_union() %>% # combine all populated area geometries into one polygon geometry
+  st_buffer(2500)%>% # 2500 m buffer around geometries
+  st_intersection(bundesland_clean) # clip to size of state
+
+# write bewohnt_buffer to a shapefile
+st_write(bewohnt_buffer, "daten_zwischenablage/bewohnt_buffer.shp", append = F)
+
+# read in bewohnt_buffer
+bewohnt_buffer <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/bewohnt_buffer.shp")
+
+#### -------------------------- Residential Areas ------------------------- ####
+# create buffer around residential areas
+# combine all residential area geometries into one polygon geometry
+wohngebiete_buffer <- fl_wid_kat_alle %>%
+  filter(kategorie == "wohngebiet") %>% # choose residential areas from df
+  select(geometry) %>% # select only the geometry column for spatial operations
+  st_union() %>% # combine all residential area geometries into one polygon geometry
+  st_buffer(500)%>% # 500 m buffer around geometries
+  st_intersection(bundesland_clean) # clip to size of state
+
+# write wohngebiete_buffer to a shapefile
+st_write(wohngebiete_buffer, "daten_zwischenablage/wohngebiete_buffer.shp", 
+         append = F)
+
+# read in wohngebiete_buffer
+wohngebiete_buffer <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/wohngebiete_buffer.shp")
+
+#### --------------------- Enterprise/Indsutrial Areas -------------------- ####
+# create buffer around enterprise/industrial areas
+# combine all enterprise/industrial area geometries into one polygon geometry
+betriebe_buffer <- fl_wid_kat_alle %>%
+  filter(kategorie == "betrieb") %>% # choose residential areas from df
+  select(geometry) %>% # select only the geometry column for spatial operations
+  st_union() %>% # combine all enterprise/industral area geometries into one polygon geometry
+  st_buffer(3500)%>% # 3500 m buffer around geometries
+  st_intersection(bundesland_clean) # clip to size of state
+
+# write betriebe_buffer to a shapefile
+st_write(betriebe_buffer, "daten_zwischenablage/betriebe_buffer.shp", 
+         append = F)
+
+# read in betriebe_buffer
+betriebe_buffer <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/betriebe_buffer.shp")
+
+
+
 
 #### --------------------------- Protected Areas -------------------------- #### 
-# combine all polygons together
+# combine all polygons together on Protected Area Data
+## use old dataframe for interactive leaflet map
 
-# check for overaps and then combine - fuse borders (here don't need buffer)
+# check validity of all geometries and correct if not
+if ("geometry" %in% names(pa_all)) {
+  
+  # Check validity of geometries
+  invalid_geoms <- st_is_valid(pa_all$geometry)
+  
+  # If there are invalid geometries, correct them using st_make_valid
+  if (any(!invalid_geoms)) {
+    # Identify and correct invalid geometries
+    pa_all$geometry[!invalid_geoms] <- st_make_valid(pa_all$geometry[!invalid_geoms])
+    
+  }
+  
+  # Use st_union to join all pa geometries into one multipolygon
+  pa_comb <- st_union(pa_all$geometry)
+  
+} 
+
+#### ------------------------------- Roads -------------------------------- ####
+# combine all road geometries into one line/multiline geometry
+# sbg_str_comb <- st_union(sbg_str_all_clean$geometry)
+
+# create a 500 m buffer around the line geometry of the road system
+# str_buffer <- st_buffer(sbg_str_comb, 500) %>%
+#   st_intersection(bundesland) (maybe with line for road? - or just combine with sbg_str_comb)
+
+
+# write road_buffer to a shapefile
+# st_write(road_buffer, "daten_zwischenablage/str_buffer.shp", append = F)
+
+# read in road_buffer
+str_buffer <- st_read("daten_zwischenablage/str_buffer.shp")
+  
+# include areas that touch the 500m buffer border
+
+#### ----------------------------- Railways (see what criteria is best) ------------------------------- ####
+# combine all rail lines geometries into one polygon
+# check validity of all geometries and correct if not
+
+# combine all road geometries into one line/multiline geometry
+sbg_bahn_comb <- st_union(sbg_str_all_clean$geometry)
+
+# create a 500 m buffer around the line geometry of the road system
+sbg_bahn_buffer <- st_buffer(sbg_bahn_comb, 500) %>%
+  st_intersection(bundesland) 
+
+
+# write str_buffer to a shapefile
+st_write(sbg_bahn_buffer, "daten_zwischenablage/bahn_buffer.shp", append = F)
+
+# read in str_buffer
+bahn_buffer <- st_read("daten_zwischenablage/bahn_buffer.shp")
+
+#### ---------------------------- Developments ---------------------------- ####
+## all developments
+# combine all developments' geometries into one polygon
+# check validity of all geometries and correct if not
+
+# combine all developments' geometries into one line/multiline geometry
+sbg_alle_geb_comb <- st_union(sbg_alle_geb_clean$geometry)
+
+# create a 2500 m buffer around the polygon geometry of all developments in populated areas
+sbg_alle_geb_buffer <- st_buffer(sbg_alle_geb_comb, 2500) %>%
+  st_intersection(bundesland) 
+
+
+# write sbg_alle_geb_buffer to a shapefile
+st_write(sbg_alle_geb_buffer, "daten_zwischenablage/alle_geb_buffer.shp", append = F)
+
+# read in road_buffer
+alle_geb_buffer <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/alle_geb_buffer.shp")
+
+## enterprises
+# combine all enterprises' geometries into one polygon
+# check validity of all geometries and correct if not
+
+# combine all road geometries into one polygon geometry
+sbg_betriebe_comb <- st_union(sbg_betriebe_clean$geometry)
+
+# create a 3500 m buffer around the polygon geometry of all enterprise areas
+sbg_betriebe_buffer <- st_buffer(sbg_betriebe_comb, 3500) %>%
+  st_intersection(bundesland) 
+
+
+# write sbg_all_geb_buffer to a shapefile
+st_write(sbg_betriebe_buffer, "daten_zwischenablage/betriebe_buffer_buffer.shp", append = F)
+
+# read in sbg_all_geb_buffer
+betriebe_buffer <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/betriebe_buffer_buffer.shp")
+
+## residential
+# combine all residential geometries into one polygon
+# check validity of all geometries and correct if not
+
+# combine all road geometries into one polygon geometry
+sbg_wohngebiete_comb <- st_union(sbg_wohngebiete_clean$geometry)
+
+# create a 3500 m buffer around the polygon geometry of all enterprise areas
+sbg_wohngebiete_buffer <- st_buffer(sbg_wohngebiete_comb, 500) %>%
+  st_intersection(bundesland) 
+
+
+# write sbg_all_geb_buffer to a shapefile
+st_write(sbg_wohngebiete_buffer, "daten_zwischenablage/wohngebiete_buffer.shp", append = F)
+
+# read in sbg_all_geb_buffer
+wohngebiete_buffer <- st_read("/vsicurl/https://raw.githubusercontent.com/tyls-27/solar_salzburg/master/daten_zwischenablage/wohngebiete_buffer.shp")
+
+#### TO DO: Railway clarify, electricity net clarify, populated areas clarify (where people live and work), and then work on raster operations / summarise the vector operations to create a mask for the raster areas
+#### populated areas should be all but industry
+
+
+#### --------------------------- Populated Areas -------------------------- ####
+tm_shape(st_transform(bundesland, "EPSG: 4326")) + tm_polygons() + tm_shape(sbg_bewohnt_osm$osm_polygons) + tm_polygons()
+
+
+#### ---------- Combine vector buffers and geometric conditions ----------- ####
+# clip betriebe_buffer
+
+#### ----------------------------- Land Cover ----------------------------- ####
+# 10 - Tree Cover
+# 20 - Shrublands # suitable
+# 30 - Grassland # suitable
+# 40 - Cropland # suitable (try with and without)
+# 50 - Built-up
+# 60 - Bare/sparse vegetation # suitable
+# 70 - Snow and ice
+# 80 - Permanent water bodies
+# 90 - Herbaceous wetland
+# 100 - Moss and lichen
+
+# clip raster to polygon buffers 
+
+# set all raster cells which are suitable to 1 and the others to na
 
 #### Topography ####
 # calculate aspect from the dsm
@@ -463,14 +729,19 @@ hangneigung <- rast("daten_zwischenablage/hangneigung.tif")
 
 # load in slope and aspect rasters
 
-#hangausrichtung <- rast("offline_files/hangausrichtung5m/hangausrichtung5m.asc")
+# hangausrichtung <- rast("offline_files/hangausrichtung5m/hangausrichtung5m.asc")
 # hangneigung <- rast("offline_files/hangneigung5m/hangneigung5m.asc")
 
 # no crs present - assign CRS based on metadata 
 # crs(hangausrichtung) <- "EPSG: 31258" # (https://service.salzburg.gv.at/ogd/client/showDetail/fbeed257-28bf-44b1-9ab4-eeb80863b79c)
 # crs(hangneigung) <- "EPSG: 31258" # https://service.salzburg.gv.at/ogd/client/showDetail/57c38869-3226-4023-8030-34b5f54e2f0b
 
+# lower than 1500 m
+
 #### --------------------------- Map and Graphs --------------------------- ####
+
+# Maps: map on PAs, map on built-up areas, map on land-use, topographical map
+# map of municipalities
 
 # see which criteria make sense and download accordingly
 # save these datasets offline as they are large (or write the shp at least onto github if fits )
